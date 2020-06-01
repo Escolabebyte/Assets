7aa94c666f25255769dba6b4a4e602f372de11ff
bebyte@bebyte.com.br <@Sil545501>
2020-06-01 14:50:34 -0300

    Initial commit

---
+++ Scripts/BasicoCSharp.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicoCSharp : MonoBehaviour
+{
+
+    public int idade;
+    public string nome;
+    public float tempo;
+    public bool temIdadeSuficiente;
+
+    public Camera camera;
+
+    public BasicoCSharp basico;
+
+    private void Awake()
+    {
+        print("Awake");
+    }
+
+    void Start()
+    {
+        idade = 0;
+        nome = "João";
+        tempo = 50.5f;
+        temIdadeSuficiente = true;
+
+        print("Start");
+
+        string[] nomes = new string[] { "Maria", "João", "Pedro" };
+
+        for (int i = 0; i < nomes.Length; i++)
+        {
+            print(nomes[i]);
+        }
+
+        foreach (var item in nomes)
+        {
+            print(item);
+        }
+
+        while (idade < 18)
+        {
+            print("Vc é menor de idade");
+            idade++;
+        }
+
+        do
+        {
+            if (idade == 18)
+                print("Vc é maior de idade");
+            else
+                print("Vc é menor de idade");
+
+            idade++;
+        } while (idade < 18);
+
+
+        camera.backgroundColor = Color.green;
+        print(basico.idade);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+
+    }
+
+    public bool ChecarSeTemIdadeSuficiente(int idade)
+    {
+        if (idade == 18) return true;
+        else return false;
+    }
+}
---
+++ Scripts/Movimetacao.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimetacao : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float velocidade;
+    private float vertical;
+    private float horizontal;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3(horizontal, rb.velocity.y, vertical) * velocidade;
+    }
+}
